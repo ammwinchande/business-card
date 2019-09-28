d19c7552cadce17246467039b6d3003c13a21a33
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2019-09-29 00:44:54 +0300

    completed personalized business card

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.brown.shade400,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 radius: 50.0,
                 backgroundColor: Colors.white,
                 backgroundImage: AssetImage('images/muhammad.jpg'),
+                // backgroundImage: NetworkImage(
+                //     'https://micim.co.uk/wp-content/uploads/2018/03/user-placeholder-square.jpg'),
               ),
               Text(
                 'Muhammad Mwinchande',
@@ -33,62 +36,59 @@ class MyApp extends StatelessWidget {
                 style: TextStyle(
                   fontFamily: 'OpenSans',
                   fontWeight: FontWeight.w500,
-                  color: Colors.teal[100],
+                  color: Colors.brown[100],
                   fontSize: 16.0,
                 ),
               ),
-              Container(
-                padding: EdgeInsets.all(10.0),
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  horizontal: 25.0,
-                  vertical: 16.0,
-                ),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.phone,
-                      color: Colors.teal,
-                    ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      '+255 679 255293',
-                      style: TextStyle(
-                        fontFamily: 'OpenSans',
-                        fontSize: 20.0,
-                        color: Colors.teal.shade900,
-                      ),
-                    ),
-                  ],
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                  thickness: 1.0,
                 ),
               ),
-              Container(
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(
+                  horizontal: 25.0,
+                  vertical: 16.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.brown,
+                  ),
+                  title: Text(
+                    '+255 679 255293',
+                    style: TextStyle(
+                      fontFamily: 'OpenSans',
+                      fontSize: 20.0,
+                      color: Colors.brown.shade900,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
                 margin: EdgeInsets.symmetric(
                   horizontal: 25.0,
                   vertical: 16.0,
                 ),
                 color: Colors.white,
-                padding: EdgeInsets.all(10.0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(
-                      Icons.email,
-                      color: Colors.teal,
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.brown,
+                  ),
+                  title: Text(
+                    'ammwinchande@gmail.com',
+                    style: TextStyle(
+                      fontFamily: 'OpenSans',
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.brown,
                     ),
-                    SizedBox(
-                      width: 10.0,
-                    ),
-                    Text(
-                      'ammwinchande@gmail.com',
-                      style: TextStyle(
-                        fontFamily: 'OpenSans',
-                        fontSize: 20.0,
-                        color: Colors.teal.shade900,
-                      ),
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ],
