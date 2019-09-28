0d86965478fa0d93d05d642ea4fc7dd3897b6248
Muhammad A. Mwinchande <ammwinchande@gmail.com>
2019-09-29 01:29:30 +0300

    add gradient customizations, and took a screenshot

--- lib/main.dart
+++ lib/main.dart
@@ -11,108 +11,119 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.brown.shade400,
-        body: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                radius: 50.0,
-                backgroundColor: Colors.white,
-                backgroundImage: AssetImage('images/muhammad.jpg'),
-                // backgroundImage: NetworkImage(
-                //     'https://micim.co.uk/wp-content/uploads/2018/03/user-placeholder-square.jpg'),
-              ),
-              Text(
-                'Muhammad Mwinchande',
-                style: TextStyle(
-                  fontFamily: 'Pacifico',
-                  fontSize: 28.0,
+        body: Container(
+          decoration: BoxDecoration(
+            color: Colors.brown,
+            gradient: LinearGradient(
+              colors: [
+                Colors.brown.shade600,
+                Colors.brown.shade200,
+              ],
+            ),
+          ),
+          child: SafeArea(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.white,
+                  backgroundImage: AssetImage('images/muhammad.jpg'),
+                  // backgroundImage: NetworkImage(
+                  //     'https://micim.co.uk/wp-content/uploads/2018/03/user-placeholder-square.jpg'),
+                ),
+                Text(
+                  'Muhammad Mwinchande',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 28.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Text(
+                  'full stack mobile and systems developer'.toUpperCase(),
+                  style: TextStyle(
+                    fontFamily: 'OpenSans',
+                    fontWeight: FontWeight.w500,
+                    color: Colors.brown[100],
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.brown.shade100,
+                    thickness: 1.0,
+                  ),
+                ),
+                Card(
                   color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              Text(
-                'full stack mobile and systems developer'.toUpperCase(),
-                style: TextStyle(
-                  fontFamily: 'OpenSans',
-                  fontWeight: FontWeight.w500,
-                  color: Colors.brown[100],
-                  fontSize: 16.0,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.brown.shade100,
-                  thickness: 1.0,
-                ),
-              ),
-              Card(
-                color: Colors.white,
-                margin: EdgeInsets.symmetric(
-                  horizontal: 25.0,
-                  vertical: 16.0,
-                ),
-                child: ListTile(
-                  leading: Icon(
-                    Icons.phone,
-                    color: Colors.brown,
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 16.0,
                   ),
-                  title: Text(
-                    '+255 679 255293',
-                    style: TextStyle(
-                      fontFamily: 'OpenSans',
-                      fontSize: 20.0,
-                      color: Colors.brown.shade900,
-                    ),
-                  ),
-                ),
-              ),
-              Card(
-                margin: EdgeInsets.symmetric(
-                  horizontal: 25.0,
-                  vertical: 16.0,
-                ),
-                color: Colors.white,
-                child: ListTile(
-                  leading: Icon(
-                    Icons.email,
-                    color: Colors.brown,
-                  ),
-                  title: Text(
-                    'ammwinchande@gmail.com',
-                    style: TextStyle(
-                      fontFamily: 'OpenSans',
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
                       color: Colors.brown,
                     ),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150.0,
-                child: Divider(
-                  color: Colors.brown.shade100,
-                  thickness: 1.0,
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(16.0),
-                child: Center(
-                  child: Text(
-                    'www.ammwinchande.me',
-                    style: TextStyle(
-                      fontFamily: 'Pacifico',
-                      color: Colors.brown.shade900,
-                      fontSize: 28.0,
+                    title: Text(
+                      '+255 679 255293',
+                      style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        fontSize: 20.0,
+                        color: Colors.brown.shade900,
+                      ),
                     ),
                   ),
                 ),
-              )
-            ],
+                Card(
+                  margin: EdgeInsets.symmetric(
+                    horizontal: 25.0,
+                    vertical: 16.0,
+                  ),
+                  color: Colors.white,
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.brown,
+                    ),
+                    title: Text(
+                      'ammwinchande@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'OpenSans',
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.brown,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                  width: 150.0,
+                  child: Divider(
+                    color: Colors.brown.shade100,
+                    thickness: 1.0,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(16.0),
+                  child: Center(
+                    child: Text(
+                      'www.ammwinchande.me',
+                      style: TextStyle(
+                        fontFamily: 'Pacifico',
+                        color: Colors.brown.shade900,
+                        fontSize: 28.0,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
